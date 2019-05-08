a65c5d0fdd8ca2fab4f6d03f1325495d733118eb
tortoiseLi <390687754@qq.com>
2019-05-08 11:28:52 +0800

    BaseController处理

--- src/main/java/org/jeecgframework/core/common/controller/BaseController.java
+++ src/main/java/org/jeecgframework/core/common/controller/BaseController.java
@@ -15,69 +15,62 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-
 /**
- * 基础控制器，其他控制器需集成此控制器获得initBinder自动转换的功能
- * @author  张代浩
- * 
+ * 基础控制器
+ * @author Administrator
+ * @date 2019-05-08
+ * @version V1.0
  */
 @Controller
 @RequestMapping("/baseController")
 public class BaseController {
 
 	/**
-	 * 将前台传递过来的日期格式的字符串，自动转化为Date类型
-	 * 
+	 * 将前台传递过来的日期格式的字符串自动转化为Date类型
 	 * @param binder
 	 */
 	@InitBinder
 	public void initBinder(ServletRequestDataBinder binder) {
-//		SimpleDateFormat dateFormat = new SimpleDateFormat(
-//				"yyyy-MM-dd hh:mm:ss");
-//		binder.registerCustomEditor(Date.class, new CustomDateEditor(
-//				dateFormat, true));
 		binder.registerCustomEditor(Date.class, new DateConvertEditor());
 	}
 
 	/**
-	 * 分页公共方法(非easyui)
-	 * 
-	 * @author Alexander
-	 * @date 20131022
+	 * 分页公共方法
+	 * @param request
+	 * @param dc
+	 * @param commonService
+	 * @param pageRow
+	 * @return
 	 */
-	public List<?> pageBaseMethod(HttpServletRequest request,
-			DetachedCriteria dc, CommonService commonService, int pageRow) {
+	public List<?> pageBaseMethod(HttpServletRequest request, DetachedCriteria dc, CommonService commonService, int pageRow) {
 		// 当前页
+		int currentPage;
+
 		// 总条数
+		int totalRow;
+
 		// 总页数
+		int totalPage;
 
-		int currentPage = 1;
-
-		int totalRow = 0;
-		int totalPage = 0;
 		// 获取当前页
 		String str_currentPage = request.getParameter("str_currentPage");
-		currentPage = str_currentPage == null || "".equals(str_currentPage) ? 1
-				: Integer.parseInt(str_currentPage);
+		currentPage = str_currentPage == null || "".equals(str_currentPage) ? 1 : Integer.parseInt(str_currentPage);
 		// 获取每页的条数
 		String str_pageRow = request.getParameter("str_pageRow");
-		pageRow = str_pageRow == null || "".equals(str_pageRow) ? pageRow
-				: Integer.parseInt(str_pageRow);
+		pageRow = str_pageRow == null || "".equals(str_pageRow) ? pageRow : Integer.parseInt(str_pageRow);
 
 		// 统计的总行数
 		dc.setProjection(Projections.rowCount());
 
-		totalRow = Integer.parseInt(commonService.findListByDetached(dc).get(0)
-				.toString());
+		totalRow = Integer.parseInt(commonService.findListByDetached(dc).get(0).toString());
 		totalPage = (totalRow + pageRow - 1) / pageRow;
 
 		currentPage = currentPage < 1 ? 1 : currentPage;
 		currentPage = currentPage > totalPage ? totalPage : currentPage;
+
 		// 清空统计函数
 		dc.setProjection(null);
-		// dc.setResultTransformer(dc.DISTINCT_ROOT_ENTITY);
-		List<?> list = commonService.findListByDetachedCriteria(dc, (currentPage - 1) * pageRow,
-				pageRow);
+		List<?> list = commonService.findListByDetachedCriteria(dc, (currentPage - 1) * pageRow, pageRow);
 
 		request.setAttribute("currentPage", currentPage);
 		request.setAttribute("pageRow", pageRow);
@@ -86,16 +79,13 @@ public class BaseController {
 		return list;
 	}
 
-    /**
-     * 抽取由逗号分隔的主键列表
-     *
-     * @param ids
-     *            由逗号分隔的多个主键值
-     * @return 主键类表
-     * @author 张国明 2014-8-21 21:57:16
-     */
-    protected List<String> extractIdListByComma(String ids) {
-        List<String> result = new ArrayList<String>();
+	/**
+	 * 抽取由逗号分隔的主键列表
+	 * @param ids
+	 * @return
+	 */
+	protected List<String> extractIdListByComma(String ids) {
+        List<String> result = new ArrayList<>();
         if (StringUtils.hasText(ids)) {
             for (String id : ids.split(",")) {
                 if (StringUtils.hasLength(id)) {
